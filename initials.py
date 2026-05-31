91b12a3d35f51fc90454d36e73907e2b4d5d61de
Justice-65 <2425403636@live.gctu.edu.gh>
2026-05-31 01:54:37 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Justice Padi Daunor
+# STUDENT ID:  2425403636
+# Email: 2425403636@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Justice Padi Daunor, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
+def main():         # write initials
+    print()
+    print("...JPD")
+    print()
+    print("...........JJJJ........PPPPPPPPPP..........DDDDDDD..........")
+    print("...........JJJJ........PPPPPPPPPPP.........DDDDDDDDD........")
+    print("...........JJJJ........PPPP....PPP.........DDD....DDD.......")
+    print("...........JJJJ........PPPP....PPP.........DDD.....DDD......")
+    print("...........JJJJ........PPPPPPPPPPP.........DDD.....DDD......")
+    print("...........JJJJ........PPPPPPPPPP..........DDD.....DDD......")
+    print("...........JJJJ........PPPP................DDD.....DDD......")
+    print("...JJJJ....JJJJ........PPPP................DDD....DDD.......")
+    print("...JJJJ...JJJJ....**...PPPP...........**...DDDDDDDDD......**")
+    print("....JJJJJJJJJ.....**...PPPP...........**...DDDDDDD........**")
 
-
-main()
